7ae097764e9795692f2b63eece973f490082f14e
A Student <student@nowhere.edu>
2026-01-16 23:24:55 +0000

    Jan 16, 2026, 3:24 PM

--- LittleComputer.cpp
+++ LittleComputer.cpp
@@ -1,3 +1,31 @@
 #include <LittleComputer.h>
 using namespace std;
 
+class LittleComputer {
+public:
+    /**
+     * @brief Construct a new Little Computer object with an empty program
+     * 
+     */
+    LittleComputer();
+
+    /**
+     * @brief Get the Program Counter
+     * 
+     * @return int Current program counter
+     */
+    int getProgramCounter();
+
+    /**
+     * @brief Get the Accumulator value
+     * 
+     * @return int Current accumulator value
+     */
+    int getAccumulator();
+    
+    // Add other public member functions / constructors here
+
+private:
+    // Add member variables here
+};
+
