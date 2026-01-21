1873a4bbccf83e69812e8655a0f768adf297c2c2
A Student <student@nowhere.edu>
2026-01-21 05:20:02 +0000

    Jan 20, 2026, 9:20 PM

--- LittleComputer.cpp
+++ LittleComputer.cpp
@@ -14,9 +14,11 @@ LittleComputer::LittleComputer(const vector<int>& instructions)
   m_accumulator = 0;
   m_programCounter = 0;
   m_memory = instructions;
+  for(instruction.size()){
+  int instruction = m_memory.at(getProgramCounter());
   if (!isHalted()) {
     switch (m_memory.at(getProgramCounter()) / 100) {
-      int instruction = m_memory.at(getProgramCounter());
+      
 
     case 0:
       cout << "bingbong";
@@ -43,7 +45,7 @@ LittleComputer::LittleComputer(const vector<int>& instructions)
     incrementCounter();
   } else {
     cout << "Error: isHalted" << endl;
-  };
+  };}
 };
 
 int LittleComputer::getMemoryAt(int location)
