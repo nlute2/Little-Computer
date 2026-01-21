ff47f137c5f8bfbefff662c0f3f57d4a690fc0ab
A Student <student@nowhere.edu>
2026-01-21 06:31:38 +0000

    Jan 20, 2026, 10:31 PM

--- LittleComputer.cpp
+++ LittleComputer.cpp
@@ -52,7 +52,7 @@ LittleComputer::LittleComputer(const vector<int>& instructions)
       // case 9:
     }
 
-    if(increment){incrementCounter();]
+    if(increment){incrementCounter();
   } else {
     cout << "Error: isHalted" << endl;
   };
