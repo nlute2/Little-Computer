926cc2ce4e23bda82c4f3f48d8b663fa4e584fff
A Student <student@nowhere.edu>
2026-01-21 05:15:16 +0000

    Jan 20, 2026, 9:15 PM

--- LittleComputer.cpp
+++ LittleComputer.cpp
@@ -1,80 +1,87 @@
 #include "LittleComputer.h"
-#include <vector>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-    LittleComputer::LittleComputer(){
-    m_accumulator = 0;
-    m_programCounter = 0;
-    };
-    LittleComputer::LittleComputer(const vector<int>& instructions){
-     m_accumulator = 0;
-      m_programCounter = 0;
-      m_memory = instructions; 
-      if(!isHalted()){
-        switch(m_memory.at(getProgramCounter())/100){
-          int instruction = m_memory.at(getProgramCounter());
+LittleComputer::LittleComputer()
+{
+  m_accumulator = 0;
+  m_programCounter = 0;
+};
+LittleComputer::LittleComputer(const vector<int>& instructions)
+{
+  m_accumulator = 0;
+  m_programCounter = 0;
+  m_memory = instructions;
+  if (!isHalted()) {
+    switch (m_memory.at(getProgramCounter()) / 100) {
+      int instruction = m_memory.at(getProgramCounter());
 
-          case 0: 
-          cout << "bingbong";
+    case 0:
+      cout << "bingbong";
 
-          case 1:
-          m_accumulator +=  getMemoryAt(instruction%100); 
-          case 2:
-          m_accumulator -= getMemoryAt(instruction % 100);
-          case 3:
-          m_memory.at(instruction%100) = m_accumulator;
-          case 5:
-          m_accumulator = getMemoryAt(instruction % 100);
-          case 6:
-          m_programCounter = getMemoryAt(instruction % 100);
-          case 7:
-          if (m_accumulator == 0){
-            m_programCounter = getMemoryAt(instruction % 100);
-          }
-         // case 8: 
-          
-         // case 9:
-
-
-        }
-
-      incrementCounter();
+    case 1:
+      m_accumulator = getAccumulator() + getMemoryAt(instruction % 100);
+    case 2:
+      m_accumulator = getAccumulator() - getMemoryAt(instruction % 100);
+    case 3:
+      m_memory.at(instruction % 100) = m_accumulator;
+    case 5:
+      m_accumulator = getMemoryAt(instruction % 100);
+    case 6:
+      m_programCounter = getMemoryAt(instruction % 100);
+    case 7:
+      if (m_accumulator == 0) {
+        m_programCounter = getMemoryAt(instruction % 100);
       }
-      else{cout << "Error: isHalted"<< endl;};
-    };
+      // case 8:
 
-    
-    int LittleComputer::getMemoryAt(int location){
-      return m_memory.at(location);
+      // case 9:
     }
-    void LittleComputer::incrementCounter(){
-      m_programCounter += 1; 
-    }
-    int LittleComputer::getCurrentInstruction(){
-      return m_memory.at(m_programCounter);
-    }
-    void LittleComputer::step(){
-      LittleComputer();
-    }
-    bool LittleComputer::isHalted(){
-      if (m_memory.at(getProgramCounter())==0){
-        return true;
-      }else{
-      return false;}
-    }
-    void LittleComputer::restart(){}
 
-  
-    int LittleComputer::getProgramCounter(){return m_programCounter;}
+    incrementCounter();
+  } else {
+    cout << "Error: isHalted" << endl;
+  };
+};
 
-   
-    int LittleComputer::getAccumulator(){
-      return m_accumulator;
-    }
-    
-    // Add other public member functions / constructors here
+int LittleComputer::getMemoryAt(int location)
+{
+  return m_memory.at(location);
+}
+void LittleComputer::incrementCounter()
+{
+  m_programCounter += 1;
+}
+int LittleComputer::getCurrentInstruction()
+{
+  return m_memory.at(m_programCounter);
+}
+void LittleComputer::step()
+{
+  LittleComputer();
+}
+bool LittleComputer::isHalted()
+{
+  if (m_memory.at(getProgramCounter()) == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+void LittleComputer::restart()
+{
+}
 
+int LittleComputer::getProgramCounter()
+{
+  return m_programCounter;
+}
 
+int LittleComputer::getAccumulator()
+{
+  return m_accumulator;
+}
 
+// Add other public member functions / constructors here
