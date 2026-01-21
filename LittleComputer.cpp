2d9b39a39943c5a8044bfbbced73f57707a260fc
A Student <student@nowhere.edu>
2026-01-21 02:50:38 +0000

    Jan 20, 2026, 6:50 PM

--- LittleComputer.cpp
+++ LittleComputer.cpp
@@ -24,7 +24,7 @@ using namespace std;
           case 2:
           m_accumulator -= m_memory.at(getProgramCounter());
           case 3:
-          m_memory.at(instruction/100)
+          m_memory.at(instruction/100)s
           case 4:
           
           case 5:
